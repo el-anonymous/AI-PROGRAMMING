00fdbe2663ecd73f148cb6c5efc747ba0aeb5277
Elvissio <onunwaelvis@gmail.com>
2025-09-25 02:30:22 +0100

    Day 1 & 2, 100 Days of Code

---
+++ test.py
@@ -0,0 +1,8 @@
+#print("My Name is Eberechukwu Onunwa")
+# Take Input from user and Combine with the available string
+#print("Hello " + input("what is your name?") + ".\n" + "you are Welcomed")
+
+print("Welcome to the Band Name Generator")
+city_name = input("What is the name of the city you grew up in? ")
+pet_name = input("What is the name of your pet? ")
+print("Your band name could be " + city_name + " " + pet_name)
