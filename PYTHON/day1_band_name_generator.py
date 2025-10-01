45a6bd348b58f6e41015e3fbc2d44f9b31736529
Elvissio <onunwaelvis@gmail.com>
2025-10-01 02:31:20 +0100

    added spaces

--- PYTHON/day1_band_name_generator.py
+++ PYTHON/day1_band_name_generator.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator")
-city_name = input("What is the name of the city you grew up in?")
-pet_name = input("What is the name of your pet?")
+city_name = input("What is the name of the city you grew up in? ")
+pet_name = input("What is the name of your pet? ")
 print("Your band name could be " + city_name + " " + pet_name)
